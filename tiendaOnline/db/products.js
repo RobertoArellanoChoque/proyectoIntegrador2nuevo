756f52160715bd80308f8f6c58142d5ed850e181
martiniano18 <martinianosanchez@udesa.edu.ar>
2022-03-30 20:49:28 -0300

    Creando la carpeta de data base y subiendo la base de datos de los productos

---
+++ tiendaOnline/db/products.js
@@ -0,0 +1,64 @@
+const books = {
+    lista : [
+        {
+            subject: "Business",
+            author: "Jordan Belfort",
+            year: 2017,
+            title: "Way of the Wolf",
+        },
+        {
+            subject: "Business",
+            author: "Renée Mauborgne, W. Chan Kim",
+            year: 2004,
+            title: "Blue Ocean Strategy",
+        },
+        {
+            subject: "Science and Technology",
+            author: "John Rossman",
+            year: 2016,
+            title: "The Amazon Way on IoT: 10 Principles for Every Leader from the World's Leading Internet of Things Strategies",
+        },
+        {
+            subject: "Science and Technology",
+            author: "Walter Isaacson",
+            year: 2021,
+            title: "The Code Breaker",
+        },
+        {
+            subject: "Thrillers",
+            author: "Heather Gudenkauf",
+            year: 2022,
+            title: "The Overnight Guest",
+        },
+        {
+            subject: "Thrillers",
+            author: "Aggie Blum Thompson",
+            year: 2021,
+            title: "I Don't Forgive You",
+        },
+        {
+           subject: "Manga",
+            author: "Hiromu Arakawa",
+            year: 2004,
+            title: "The Art of Fullmetal Alchemist 2",
+        },
+        {
+            subject: "Manga",
+            author: "Naruto, Volume 6: Predator",
+            year: 2001,
+            title: "Masashi Kishimoto",
+        },
+        {
+           subject: "Personal Growth",
+            author: "Jay Shetty",
+            year: 2020,
+            title: "Think Like a Monk",
+        },
+        {
+            subject: "Personal Growth",
+            author: "Eckhart Tolle",
+            year: 1997,
+            title: "The Power of Now",
+        },
+    ]};
+module.exports= books
