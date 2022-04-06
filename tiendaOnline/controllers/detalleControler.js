1a6a29c44161d9f094a17a475b50c6519ca7683a
SchmunkJuan <jschmunk@udesa.edu.ar>
2022-04-06 08:25:08 -0300

    arranque los controllers

--- tiendaOnline/controllers/detalleControler.js
+++ tiendaOnline/controllers/detalleControler.js
@@ -1,5 +1,24 @@
 const data = require("../db/products");
+const detalleControlerr = {
+  index: function (req, res) {
+    return res.render('index', {lista: data.lista});
+  },
+  show: function (req, res) {
+       id = req.params.id;
+    let resultado = "";
 
-const detalleControler = {
-    
+    if (id > data.lista.length) {
+      resultado = "No se encontro el id solicitado";
+      return res.render("error", {message: resultado});
+    }
+    for (let i = 0; i < data.lista.length; i += 1) {
+      if (data.lista[i].id == id) {
+        resultado = `Libros Solicitados <strong>${data.lista[i].nombre}</strong>`;
+        return res.render("libro", {libro: resultado});
+      }
+    }
+
+    return res.send(resultado);
+  },
 }
+module.exports = detalleControler;
