47f777c9f5c02023a8a94ec6ba61f214e93e910b
martiniano18 <martinianosanchez@udesa.edu.ar>
2022-04-11 00:13:00 -0300

    completando db de comentarios

---
+++ tiendaOnline/db/comments.js
@@ -0,0 +1,45 @@
+const comments = {
+    lista: [
+        {
+            user: "Juan Gonzalez",
+           comment: "",
+        },
+        {
+            user: "Lucas Farias",
+            comment: "",
+        },
+        {
+            user: "Mariana Garcia",
+            comment: "",
+        },
+        {
+            user: "Maria Monteverde",
+            comment: "",
+        },
+        {
+            user: "Julian Rodriguez",
+            comment: "",
+        },
+        {
+            user: "Marta Vazquez",
+            comment: "",
+        },
+        {
+            user: "Mario Cespedes",
+            comment: "",
+        },
+        {
+            user: "Federico Palermo",
+            comment: "",
+        },
+        {
+            user: "Juana Marineli",
+            comment: "",
+        },
+        {
+
+            user: "Agustina Villalba",
+            comment: "",
+        },
+    ]
+};
