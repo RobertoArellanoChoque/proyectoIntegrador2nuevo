a7031e64f2634077ee2e6fb97512523cda4254e5
martiniano18 <martinianosanchez@udesa.edu.ar>
2022-04-13 09:02:31 -0300

    Agregue imagenes

--- tiendaOnline/db/user.js
+++ tiendaOnline/db/user.js
@@ -2,11 +2,11 @@ const user= {
     name: "Juan",
     surname: "Gonzalez",
     phone: 1133556682,
-    age: 19,
+    age: 55,
     email: "juangonzalez33@gmail.com",
     password: "aguanteboca1212",
-    idNumber: 44546781,
-    profilePhoto: "",
+    idNumber: 22546781,
+    profilePhoto: "/images/products/img-profile",
 
 };
 // puse los datos que pide en el formulario de usuarios
