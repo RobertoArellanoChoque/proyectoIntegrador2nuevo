b5d526498b0178c20732ad4e14ae5e6129c8b018
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-04-14 16:04:07 -0300

    Arreglos para las vistas

--- tiendaOnline/controllers/mainController.js
+++ tiendaOnline/controllers/mainController.js
@@ -3,7 +3,7 @@ const products = require("../db/products")
 
 const controlador = {
     index: function(req, res){
-        return res.render('index',)
+        return res.render('index',{titulo: products.lista})
     },
     show: function(req, res){
         return res.send("Aqui va el menu ya logeado")
@@ -11,6 +11,7 @@ const controlador = {
 
 
 };
+
 
 
 
