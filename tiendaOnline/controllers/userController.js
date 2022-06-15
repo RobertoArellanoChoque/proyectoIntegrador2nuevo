b44dd4637db9426bfe8787853a48ec729bb283a8
SchmunkJuan <jschmunk@udesa.edu.ar>
2022-06-15 08:46:55 -0300

    bcrypt

--- tiendaOnline/controllers/userController.js
+++ tiendaOnline/controllers/userController.js
@@ -17,3 +17,43 @@ const controladorUsuarios = {
 
 
 module.exports = controladorUsuarios
+
+let registerController ={
+	index: function(req, res){
+		res.render('register')
+	},
+	store: function(req, res){
+		let errors ={}
+		if(req.body.email == ""){
+			errors.messaje = "el email es obligatorio";
+			console.log(errors) //guarda errors en locals
+			return res.render('register')
+		} else if(req.body.password == ""){
+			errors.messaje = "la contrasena es obligatorio";
+			console.log(errors) //guarda errors en locals
+			return res.render('register')
+		} else if(req.body.retyprPassword == ""){
+			errors.messaje = "la contrasena es obligatorio";
+			console.log(errors) //guarda errors en locals
+			return res.render('register')
+		} else if(req.password != req.retyprPassword){
+			errors.messaje = "las contrasena no coinciden";
+			console.log(errors) //guarda errors en locals
+			return res.render('register')
+		}
+
+	}
+}
+module.exports = registerController
+
+const bcrypt = require('bcryptjs'); 
+const db = require('../database/user.js');
+
+let passEncriptada = bcrypt.hashSync('1234', 10);
+
+db.Usuario.create({
+	name: "Manuel",
+	username: "manolito",
+	password: passEncriptada
+});
+
