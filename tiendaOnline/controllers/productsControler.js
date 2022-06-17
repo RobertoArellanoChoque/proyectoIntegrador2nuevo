f59500847d5a6647fa9a2685753eeecd5aba711f
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-06-17 10:30:42 -0300

    Arreglos de choque

--- tiendaOnline/controllers/productsControler.js
+++ tiendaOnline/controllers/productsControler.js
@@ -1,11 +1,6 @@
 const db = require('../database/models');
-<<<<<<< HEAD
 const op = db.Sequelize.Op;
 
-=======
-//const upload = multer({ dest: '/usuarios' })
-const path = require('path');
->>>>>>> 610dbbd0305f3097f54fb63e17d7cf8db334fdd9
 module.exports = {
     index: function (req, res) {
         return res.send('Hola mundo');
