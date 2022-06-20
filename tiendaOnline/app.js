dfcb2abeb30cd498c84acbfeb6a62fc2bdd8ce50
Martiniano18 <martinianosanchez@udesa.edu.ar>
2022-06-20 18:29:47 -0300

    dd

--- tiendaOnline/app.js
+++ tiendaOnline/app.js
@@ -33,7 +33,41 @@ app.use("/", indexRouter);
 app.use("/productos", productosRouter);
 app.use("/usuario", usuarioRouter);
 
+app.use(session({
+  secret: 'tiendaOnline',
+  resave: false,
+  saveUninitialized: true
 
+}));
+
+// pasar datos de session a locals
+app.use(function(req,res,next){
+
+  res.locals.user = req.session.user
+
+  return next()
+})
+
+app.use(function(req, res, next){
+  //chequear que no tengamos usuario en sessión y si tengamos cookie
+  if(req.session.user == undefined && req.cookies.userId !== undefined){
+    //Buscar el usario de la base de datos
+       db.User.findByPk(req.cookies.userId)
+            .then( function(user){
+              //Dentro del then pasar al usario a req.session.user
+              //Pasar al usuario locals.user
+              // retornar next()
+                req.session.user = user
+                res.locals.user = user
+              
+                return next()
+
+            })
+            .catch( error => console.log(error))
+          } else { //tiene que haber un else
+            return next()
+          }
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
